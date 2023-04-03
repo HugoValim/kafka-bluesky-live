aac8749863c71942029552ec4981ec0b4828b628
Valim <hugo.campos@lnls.br>
2023-04-03 09:41:13 -0300

    Reestructured code and added a background to the interface

--- kafka_bluesky_live/live_view.py
+++ kafka_bluesky_live/live_view.py
@@ -1,21 +1,15 @@
-#!/usr/bin/env python3
-
-from os import path
 import threading
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QFrame
 
 from kafka import KafkaConsumer
 import msgpack
 
 from .widgets.live_view_tab import LiveViewTab, LiveViewInputs
+from .ui.main_window_ui import MainUI
 
-
-class LiveView(QWidget):
+class LiveView(QWidget, MainUI):
     run_start_signal = QtCore.pyqtSignal()
     run_stop_signal = QtCore.pyqtSignal()
     # update_bar_signal = QtCore.pyqtSignal()
@@ -87,15 +81,6 @@ class LiveView(QWidget):
             # self.points_now += 1
             # self.update_bar_signal.emit()
 
-    def initUI(self) -> None:
-        """Init base UI components"""
-        self.title = "Queue Server Live View"
-        height = 800
-        width = int(height * 16 / 9)
-        self.resize(width, height)
-        self.setWindowTitle(self.title)
-        self.build_main_screen_layout()
-
     def make_connections(self) -> None:
         """Connect signals to slots"""
 
@@ -105,72 +90,6 @@ class LiveView(QWidget):
 
         # QListWidget Signals
         self.list_widget.currentItemChanged.connect(self.change_stack_widget_index)
-
-    def build_icons_pixmap(self):
-        """Build used icons"""
-        img_size = 150
-        pixmap_path = path.join(path.dirname(path.realpath(__file__)), "icons")
-        self.cnpem_icon = QtGui.QPixmap(path.join(pixmap_path, "cnpem.png"))
-        self.cnpem_icon = self.cnpem_icon.scaled(
-            img_size, img_size, QtCore.Qt.KeepAspectRatio
-        )
-        self.lnls_icon = QtGui.QPixmap(path.join(pixmap_path, "lnls-sirius.png"))
-        self.lnls_icon = self.lnls_icon.scaled(
-            img_size, img_size, QtCore.Qt.KeepAspectRatio
-        )
-
-    def build_initial_screen_widget(self) -> None:
-        """Build the main screen to be displayed before a scan start"""
-        self.build_icons_pixmap()
-
-        grid_layout = QtWidgets.QGridLayout()
-
-        title_label = QtWidgets.QLabel(self)
-        title_label.setText("Bluesky Queueserver Live View")
-        title_label.setStyleSheet("font-weight: bold; font-size: 30pt")
-        title_label.setAlignment(Qt.AlignCenter)
-
-        waiting_label = QtWidgets.QLabel(self)
-        waiting_label.setText("Wainting for a scan to begin ...")
-        waiting_label.setStyleSheet("font-weight: bold; font-size: 18pt")
-        waiting_label.setAlignment(Qt.AlignCenter)
-
-        cnpem_img_label = QtWidgets.QLabel(self)
-        cnpem_img_label.setPixmap(self.cnpem_icon)
-        cnpem_img_label.setAlignment(Qt.AlignCenter)
-
-        lnls_img_label = QtWidgets.QLabel(self)
-        lnls_img_label.setPixmap(self.lnls_icon)
-        lnls_img_label.setAlignment(Qt.AlignCenter)
-
-        grid_layout.addWidget(title_label, 0, 1)
-        grid_layout.addWidget(lnls_img_label, 1, 2)
-        grid_layout.addWidget(cnpem_img_label, 1, 0)
-        grid_layout.addWidget(waiting_label, 2, 1)
-
-        main_screen_widget = QFrame()
-        main_screen_widget.setLayout(grid_layout)
-
-        return main_screen_widget
-
-    def build_main_screen_layout(self):
-        self.stack_widget = QtWidgets.QStackedWidget(self)
-
-        self.frame_main = QFrame()
-        self.frame_main.setFrameShape(QFrame.StyledPanel)
-
-        self.list_widget = QtWidgets.QListWidget(self)
-        self.list_widget.setFixedWidth(250)
-        self.list_widget.addItem("main")
-
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self)
-        self.horizontalLayout.addWidget(self.list_widget)
-        self.horizontalLayout.addWidget(self.frame_main)
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.frame_main.setLayout(self.verticalLayout)
-
-        self.verticalLayout.addWidget(self.stack_widget)
-        self.stack_widget.addWidget(self.build_initial_screen_widget())
 
     def change_stack_widget_index(self):
         self.stack_widget.setCurrentIndex(self.list_widget.currentRow())
